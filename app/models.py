bbd167a40cfe6e86e597ddebcd57040cf285a030
Pherang <pherang@users.noreply.github.com>
2018-07-01 00:07:01 -0400

    More testing of models for database

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     exercises = db.relationship('Exercise', backref='user', lazy=True)
+    def __repr__(self):
+       return '<User {}>'.format(self.username) 
 
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +13,7 @@ class Exercise(db.Model):
     duration = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Exercise {}, {}, {}, {}>'.format(self.description,
+                                                  self.duration, self.date, self.user)
--- exercisetracker.py
+++ exercisetracker.py
@@ -1,2 +1,6 @@
+from app.models import User, Exercise
+from app import webapp, db
 
-from app import webapp
+@webapp.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User': User, 'Exercise': Exercise}
